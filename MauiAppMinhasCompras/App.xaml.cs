33562d42b37baab31d49004ec91c0ab1e3dd0928
Marcel-Mattos <marcel_mattos@hotmail.com>
2025-03-10 09:23:40 -0300

    Preparando insert para adicionar os produtos a lista de compras

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db; //_db é um campo no app que vai ser preenchido
+
+        public static SQLiteDatabaseHelper Db //Db é uma propriedade, para acessar a informação colocada no campo
+        {
+            get //quando pegar o Db ele retorna _db
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
